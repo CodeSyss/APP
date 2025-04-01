1749867db7fc6c4ca20160d405dfa17d87797c6b
Carlos Hernández <carlos-hernandez@correo.unimet.edu.ve>
2025-03-31 21:44:57 -0400

    modifications

--- backend_nest/src/app.module.ts
+++ backend_nest/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProductModule, UserModule],
   controllers: [],
   providers: [PrismaService],
 })
